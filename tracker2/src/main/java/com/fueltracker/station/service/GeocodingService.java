9950f15747727da3bddfcaa87dfacef6f0bf8ad7
itzHassanSH <mhshahzadpk@gmail.com>
2026-07-31 12:11:23 +0200

    GeocodingService has the GeocodingClient as an object (injected via Spring dependency injection) which essentially gets the response and returns a Coordinate object (crafted from the returned NominatimResponse)
    - essentially handles the response, taking care of error too (i.e. throws LocationNotFound exception)

--- tracker2/src/main/java/com/fueltracker/station/service/GeocodingService.java
+++ tracker2/src/main/java/com/fueltracker/station/service/GeocodingService.java
@@ -1,9 +1,29 @@
 package com.fueltracker.station.service;
 
+import com.fueltracker.dto.GeocodingApi.NominatimResponse;
+import com.fueltracker.exceptions.LocationNotFound;
 import com.fueltracker.shared.Coordinates;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class GeocodingService {
-    public Coordinates geocode(String location);
+    private final GeocodingClient client;
+
+    public GeocodingService(GeocodingClient client) {
+        this.client = client;
+    }
+
+    public Coordinates geocode(String location) {
+        List<NominatimResponse> responses = client.search(location);
+
+        if (responses.isEmpty()) {
+            throw new LocationNotFound(location);
+        }
+
+        NominatimResponse response = responses.getFirst();
+
+        return new Coordinates(Double.parseDouble(response.lon()), Double.parseDouble(response.lat()));
+    }
 }
